fb11d38aa5abf84d5165abd96186d22c3f09c953
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-01-31 17:44:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {
   sonia: 'Where are you?',
   diana: 'I am lost'
 }
-function updatedList (obj,key,value) {
-  return obj.key = 'value';
+function updatedList (playlist, artistName, songTitle) {
+  playlist.artistName = 'songTitle';
+  return playlist;
 }
